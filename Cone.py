c7b2147145e92b47f9008cc92b05ed5605d11af9
Corey Wood <coreymwood@gmail.com>
2015-06-17 11:10:35 -0500

    Added velocity getters/setters.

--- Cone.py
+++ Cone.py
@@ -36,25 +36,32 @@ class Cone:
                 falloff --  Radial density distribution exponent. Default is 1 for
                             a mass-conserving outflow (density goes as r^-2).
                             A value of 1/3 creates a constant-density profile.
+
+            Returns:
+                None. Creates "positions" member variable, containing Cartesian
+                position vectors for 'n' clouds, and "velocities" member variable,
+                containing zero-velocity vectors for all clouds (ostensibly these
+                are Cartesian as well).
         """
+        self._n = n
 
         # Even spread in cos(theta) to avoid clustering at poles.
         theta_rad = np.radians(self.theta)
         if bicone:
-            vs_1 = np.random.random(n/2.) * \
+            vs_1 = np.random.random(self._n/2.) * \
                    (1-np.cos(theta_rad)) + np.cos(theta_rad)
-            vs_2 = -(np.random.random(n/2.) * \
+            vs_2 = -(np.random.random(self._n/2.) * \
                     (1-np.cos(theta_rad)) + np.cos(theta_rad))
             vs = np.concatenate((vs1, vs2))
         else:
-            vs = np.random.random(n) * (1-np.cos(theta_rad)) + np.cos(theta_rad)
+            vs = np.random.random(self._n) * (1-np.cos(theta_rad)) + np.cos(theta_rad)
         thetas = np.arccos(vs)
 
-        us = np.random.random(n)
+        us = np.random.random(self._n)
         phis = us * np.pi * 2.0
 
         #falloff = 1 # 1/3 ~ constant density, 1/2 ~ 1/r fall-off, 1 ~ r^-2 fall-off
-        rs = np.random.random(n)**falloff * (self.r_out - self.r_in) + self.r_in
+        rs = np.random.random(self._n)**falloff * (self.r_out - self.r_in) + self.r_in
 
         # Still need to rotate them!
         self._sph_pts = np.vstack((rs, thetas, phis)).transpose()
@@ -68,9 +75,10 @@ class Cone:
         # Rot in -X for inc (or rotate -inc in +X) and in Z for PA
         self.positions = np.asarray([Rot(pt, x=-self.inc, z=self.PA) \
                             for pt in cart_pts])
+        self.velocities = np.zeros_like(self.positions)
 
-    # Properties for nicely slicing the 2D array of coordinates into lists of each
-    # of your normal spherical coordinates.
+    # Properties for nicely slicing the 2D array of positions and velocities
+    # into lists for each coordinate.
     @property
     def _rs(self):
         return self._sph_pts[:,0]
@@ -94,6 +102,36 @@ class Cone:
     @property
     def _zs(self):
         return self.positions[:,2]
+
+    @property
+    def _vxs(self):
+        return self.velocities[:,0]
+    @_vxs.setter
+    def _vxs(self, vxs):
+        if np.shape(vxs) == np.shape(self._vxs):
+            self.velocities[:,0] = vxs
+        else:
+            raise Exception("Array is not the same length as self._vxs.")
+
+    @property
+    def _vys(self):
+        return self.velocities[:,1]
+    @_vys.setter
+    def _vys(self, vys):
+        if np.shape(vys) == np.shape(self._vys):
+            self.velocities[:,1] = vys
+        else:
+            raise Exception("Array is not the same length as self._vys.")
+
+    @property
+    def _vzs(self):
+        return self.velocities[:,2]
+    @_vzs.setter
+    def _vzs(self, vzs):
+        if np.shape(vzs) == np.shape(self._vzs):
+            self.velocities[:,2] = vzs
+        else:
+            raise Exception("Array is not the same length as self._vzs.")
 
 if __name__ == "__main__":
     pass
